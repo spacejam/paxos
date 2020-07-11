7a6cccc6569edd25e13061a36f96c3d738f80c3a
Tyler Neely <t@jujit.su>
2020-07-11 19:36:54 +0200

    Update sled, small refactoring

--- src/udp_transport.rs
+++ src/udp_transport.rs
@@ -1,13 +1,12 @@
 use std::{
     convert::TryInto,
     io,
-    net::{ToSocketAddrs, UdpSocket},
+    net::{SocketAddr, ToSocketAddrs, UdpSocket},
 };
 
 use bincode::{deserialize, serialize};
 use crc32fast::Hasher;
-
-use crate::{Reactor, Transport};
+use serde::{de::DeserializeOwned, Serialize};
 
 const MAX_SZ: usize = 64 * 1024;
 
@@ -18,17 +17,22 @@ pub struct UdpTransport {
 
 impl UdpTransport {
     /// Create a new UdpTransport
-    pub fn new<A: ToSocketAddrs>(addr: A) -> io::Result<UdpTransport> {
+    pub fn new<A: ToSocketAddrs>(
+        addr: A,
+    ) -> io::Result<UdpTransport> {
         let socket = UdpSocket::bind(addr)?;
         Ok(UdpTransport { socket })
     }
 }
 
-impl<R: Reactor<Peer = String>> Transport<R> for UdpTransport {
+impl UdpTransport {
     /// Blocks until the next message is received.
-    fn next_message(&mut self) -> (R::Peer, R::Message) {
+    fn next_message<M: DeserializeOwned>(
+        &mut self,
+    ) -> (SocketAddr, M) {
         let mut buf = [0; MAX_SZ];
-        let (n, from) = self.socket.recv_from(&mut buf).unwrap();
+        let (n, from) =
+            self.socket.recv_from(&mut buf).unwrap();
 
         let crc_sz = std::mem::size_of::<u32>();
         let data_buf = &buf[..n - crc_sz];
@@ -38,15 +42,20 @@ impl<R: Reactor<Peer = String>> Transport<R> for UdpTransport {
         hasher.update(&data_buf);
         let hash = hasher.finalize();
 
-        let crc_array: [u8; 4] = crc_buf.try_into().unwrap();
+        let crc_array: [u8; 4] =
+            crc_buf.try_into().unwrap();
         assert_eq!(u32::from_le_bytes(crc_array), hash);
 
-        let msg: R::Message = deserialize(&buf[..n]).unwrap();
-        (from.to_string(), msg)
+        let msg: M = deserialize(&buf[..n]).unwrap();
+        (from, msg)
     }
 
     /// Enqueues the message to be sent. May be sent 0-N times with no ordering guarantees.
-    fn send_message(&mut self, to: R::Peer, msg: R::Message) {
+    fn send_message<M: Serialize>(
+        &mut self,
+        to: SocketAddr,
+        msg: M,
+    ) {
         let mut serialized = serialize(&msg).unwrap();
         let mut hasher = Hasher::new();
         hasher.update(&serialized);
@@ -54,7 +63,8 @@ impl<R: Reactor<Peer = String>> Transport<R> for UdpTransport {
         serialized.copy_from_slice(&hash.to_le_bytes());
         assert!(serialized.len() <= MAX_SZ);
 
-        let n = self.socket.send_to(&serialized, to).unwrap();
+        let n =
+            self.socket.send_to(&serialized, to).unwrap();
         assert_eq!(n, serialized.len());
     }
 }
